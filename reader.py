2b0f7e0aa3a3c4476aac19c28afccf6fd3efb56a
Eli <elimermz@gmail.com>
2026-02-10 16:07:41 +0200

    Complete Stage 1 tasks

---
+++ reader.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+def file_to_list(file_path)->list[list[str]]:
+    with open(file_path,"r") as file:
+        return [packet.strip().split(",") for packet in file.readlines()]
+
+print(file_to_list("test.log"))
