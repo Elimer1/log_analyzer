543864a17e8a8d31301db1991d255c4942761050
Eli <elimermz@gmail.com>
2026-02-10 16:33:50 +0200

    added portr to protocol function

--- reader.py
+++ reader.py
@@ -4,5 +4,7 @@ def file_to_list(file_path)->list[list[str]]:
     with open(file_path,"r") as file:
         return [packet.strip().split(",") for packet in file.readlines()]
 
+def port_to_protocol(data):
+    return {list[3]:list[4] for list in data}
 
 print(file_to_list("test.log"))
